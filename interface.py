cdf73788d64d8e8262911c13637c0fc37c3aca80
sealfury <willspecker@gmail.com>
2021-02-19 15:49:43 +0100

    creates methods for button commands for view, search, and add

--- interface.py
+++ interface.py
@@ -1,6 +1,37 @@
 from tkinter import *  # pylint: disable=unused-wildcard-import
 import backend
 
+
+def view_command():
+    list1.delete(0, END)
+    for row in backend.view():
+        list1.insert(END, row)
+
+
+def search_command():
+    list1.delete(0, END)
+    for row in backend.search(
+        title_text.get(), author_text.get(), year_text.get(), isbn_text.get()
+    ):
+        list1.insert(END, row)
+
+
+def add_command():
+    backend.insert(
+        title_text.get(), author_text.get(), year_text.get(), isbn_text.get()
+    )
+    list1.delete(0, END)
+    list1.insert(
+        END,
+        (
+            title_text.get(),
+            author_text.get(),
+            year_text.get(),
+            isbn_text.get(),
+        ),
+    )
+
+
 window = Tk()
 
 l1 = Label(window, text="Title")
@@ -40,13 +71,13 @@ sb1.grid(row=2, column=2, rowspan=6)
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
-b1 = Button(window, text="View All", width=12)
+b1 = Button(window, text="View All", width=12, command=view_command)
 b1.grid(row=2, column=3)
 
-b2 = Button(window, text="Search Entry", width=12)
+b2 = Button(window, text="Search Entry", width=12, command=search_command)
 b2.grid(row=3, column=3)
 
-b3 = Button(window, text="Add Entry", width=12)
+b3 = Button(window, text="Add Entry", width=12, command=add_command)
 b3.grid(row=4, column=3)
 
 b4 = Button(window, text="Update Selected", width=12)
