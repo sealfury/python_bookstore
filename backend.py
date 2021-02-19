ef06bb2045554bdc64a0d155e3a67cd3664b2267
sealfury <willspecker@gmail.com>
2021-02-19 14:56:09 +0100

    adds search and delete methods

--- backend.py
+++ backend.py
@@ -10,12 +10,17 @@ def connect():
     conn.commit()
     conn.close()
 
+
 def insert(title, author, year, isbn):
     conn = sqlite3.connect("books.db")
     cur = conn.cursor()
-    cur.execute("INSERT INTO book VALUES (NULL, ?, ?, ?, ?)", (title, author, year, isbn))
+    cur.execute(
+        "INSERT INTO book VALUES (NULL, ?, ?, ?, ?)",
+        (title, author, year, isbn),
+    )
     conn.commit()
     conn.close()
+
 
 def view():
     conn = sqlite3.connect("books.db")
@@ -24,5 +29,26 @@ def view():
     rows = cur.fetchall()
     conn.close()
     return rows
+
+
+def search(title="", author="", year="", isbn=""):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?",
+        (title, author, year, isbn),
+    )
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+
+def delete(id):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM book WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+
 
 connect()
