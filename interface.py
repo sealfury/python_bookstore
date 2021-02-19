21e4f61ba9542fae412a78f7facd5e2b727fd519
sealfury <willspecker@gmail.com>
2021-02-19 16:29:45 +0100

    finalizing interface command methods

--- interface.py
+++ interface.py
@@ -2,6 +2,23 @@ from tkinter import *  # pylint: disable=unused-wildcard-import
 import backend
 
 
+def get_selected_row(event):
+    global selection
+    try:
+        index = list1.curselection()[0]
+        selection = list1.get(index)
+        e1.delete(0, END)
+        e1.insert(END, selection[1])
+        e2.delete(0, END)
+        e2.insert(END, selection[2])
+        e3.delete(0, END)
+        e3.insert(END, selection[3])
+        e4.delete(0, END)
+        e4.insert(END, selection[4])
+    except IndexError:
+        pass
+
+
 def view_command():
     list1.delete(0, END)
     for row in backend.view():
@@ -32,7 +49,23 @@ def add_command():
     )
 
 
+def delete_command():
+    backend.delete(selection[0])
+
+
+def update_command():
+    backend.update(
+        selection[0],
+        title_text.get(),
+        author_text.get(),
+        year_text.get(),
+        isbn_text.get(),
+    )
+
+
 window = Tk()
+
+window.wm_title("Python Bookstore")
 
 l1 = Label(window, text="Title")
 l1.grid(row=0, column=0)
@@ -71,6 +104,8 @@ sb1.grid(row=2, column=2, rowspan=6)
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
+list1.bind("<<ListboxSelect>>", get_selected_row)
+
 b1 = Button(window, text="View All", width=12, command=view_command)
 b1.grid(row=2, column=3)
 
@@ -80,13 +115,13 @@ b2.grid(row=3, column=3)
 b3 = Button(window, text="Add Entry", width=12, command=add_command)
 b3.grid(row=4, column=3)
 
-b4 = Button(window, text="Update Selected", width=12)
+b4 = Button(window, text="Update Selected", width=12, command=update_command)
 b4.grid(row=5, column=3)
 
-b5 = Button(window, text="Delete Selected", width=12)
+b5 = Button(window, text="Delete Selected", width=12, command=delete_command)
 b5.grid(row=6, column=3)
 
-b6 = Button(window, text="Close", width=12)
+b6 = Button(window, text="Close", width=12, command=window.destroy)
 b6.grid(row=7, column=3)
 
 window.mainloop()
